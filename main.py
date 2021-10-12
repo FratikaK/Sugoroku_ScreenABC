6c368197225df124fabee784779d47643fa706d7
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-10-13 00:18:42 +0900

    [add]インスタンス追加

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import pygame
 from pygame.locals import *
 # from Screen_abc import Screen
 from ConfigScreen import ConfigScreen
+from LoadingScreen import NowLoading
 from TestScreen import TestScreen
 from TestScreen2 import TestScreen2
 import Screen_abc as SC
 
-screen = [TestScreen(), TestScreen2(), ConfigScreen()]
+screen = [TestScreen(), TestScreen2(), ConfigScreen(), NowLoading()]
 
 if __name__ == '__main__':
     while True:
