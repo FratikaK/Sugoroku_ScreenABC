2a1ab8d287a069ba6f8582f3608dd36fdc1f924c
FratikaK <kamunyan17@gmail.com>
2021-10-14 14:47:29 +0900

    [add]effect util

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import pygame
-from pygame.locals import *
 # from Screen_abc import Screen
-import TestEffect
 from ConfigScreen import ConfigScreen
 from LoadingScreen import NowLoading
 from TestScreen import TestScreen
 from TestScreen2 import TestScreen2
 import Screen_abc as SC
+from TestEffectScreen import TestEffectScreen
+from effect import effect_group
 
-screen = [TestScreen(), TestScreen2(), ConfigScreen(), NowLoading()]
+
+screen = [TestScreen(), TestScreen2(), ConfigScreen(), NowLoading(), TestEffectScreen()]
 
 if __name__ == '__main__':
     while True:
         screen[SC.ScreenNum].display()
-        effect_screen.display()
         screen[SC.ScreenNum].getEvent()
+
