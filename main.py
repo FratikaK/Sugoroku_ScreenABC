5d039f0fca66eecc71505e6ac417857dc50ccb64
FratikaK <kamunyan17@gmail.com>
2021-10-14 09:40:03 +0900

    [delete]エフェクト

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from pygame.locals import *
 # from Screen_abc import Screen
 import TestEffect
 from ConfigScreen import ConfigScreen
-from EffectScreen import EffectScreen
 from LoadingScreen import NowLoading
 from TestScreen import TestScreen
 from TestScreen2 import TestScreen2
 import Screen_abc as SC
 
-screen = [TestScreen(), TestScreen2(), ConfigScreen(), NowLoading(), TestEffect.TestDisplay()]
-effect_screen = EffectScreen()
+screen = [TestScreen(), TestScreen2(), ConfigScreen(), NowLoading()]
 
 if __name__ == '__main__':
     while True:
